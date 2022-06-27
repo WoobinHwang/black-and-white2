ae3b4815ba4cafc80f1db239849ef91565bb2bd8
WoobinHwang <dnqls0326@naver.com>
2022-06-28 01:22:51 +0900

    m

--- app/main.py
+++ app/main.py
@@ -169,26 +169,23 @@ def submitnumber():
     
     # # 상대가 없을 때
     if (len(enemy_rows) == 0):
-            result = "상대방이 없습니다."
+            result = "상대방이 들어오지 않았습니다."
     # # 보유 포인트보다 더 많이 제출하려고 할 때
     elif (user_last_rows[4]-int(text) < 0) or (int(text) < 0) :
         result = "현재 남은 포인트는 %d개입니다.\n다시 제출하세요." %(user_last_rows[4])
-    # # 문제없음
 
     else:
-        # if (len(user_last_rows) == 7):
-            # result = "내 길이: %d\n상대 길이: %d" %(len(user_rows), len(enemy_rows))
         
         # # 숫자 정상적으로 제출
         # # 길이가 같을 때 입력한 사람이 제출
-        # if(len(user_rows) == len(enemy_rows)) :
-        #     cur.execute("INSERT INTO blackwhite3 (userid, channel, score, turn, numbers, usenum) VALUES (%s, %s, %s, %s, %s, %s);"
-        #         , (id_data, channel_data, user_last_rows[2], user_last_rows[3] + 1, 200 - int(text), int(text)) )
-        #     db.commit()
-        #     result = "%s라운드 제출 완료!" %(user_last_rows[3] + 1)
-        # elif(len(user_rows) > len(enemy_rows)):
-        #     result = "상대방이 제출 할 차례입니다."
-        if(len(user_rows) < len(enemy_rows)):
+        if(len(user_rows) == len(enemy_rows)) :
+            cur.execute("INSERT INTO blackwhite3 (userid, channel, score, turn, numbers, usenum) VALUES (%s, %s, %s, %s, %s, %s);"
+                , (id_data, channel_data, user_last_rows[2], user_last_rows[3] + 1, user_last_rows[4] - int(text), int(text)) )
+            db.commit()
+            result = "%s라운드 제출 완료!" %(user_last_rows[3] + 1)
+        elif(len(user_rows) > len(enemy_rows)):
+            result = "상대방이 제출 할 차례입니다."
+        elif(len(user_rows) < len(enemy_rows)):
             
             
             # # 양쪽 다 제출했으니 숫자를 비교 할 예정
@@ -240,228 +237,3 @@ def submitnumber():
     }
 
     return responseBody
-
-
-
-
-# # # 오늘의 만족도
-# @app.route('/api/todayscore', methods=['POST'])
-# def todayscore():
-
-#     # date_data = "'%s'" %(datetime.today().strftime('%Y-%m-%d'))
-#     cur.execute("SELECT * FROM score;")
-#     rows = cur.fetchall()
-
-#     today_score = 0
-#     for i in range(len(rows)):
-#         today_score = today_score + int(rows[i][2])
-#     today_score = round(today_score / len(rows), 2)
-
-#     responseBody = {
-#         "version": "2.0",
-#         "template": {
-#             "outputs": [
-#                 {
-#                     "simpleText": {
-#                         "text": "평균 만족도는 %s점이며\n총 %s명이 만족도 점수를 눌러주셨습니다." % (today_score, len(rows))
-#                     }
-#                 }
-#             ]
-#         }
-#     }
-
-#     return responseBody
-
-# # # 모의주식 구매
-# @app.route('/api/buyitem', methods=['POST'])
-# def buyitem():
-
-#     body = request.get_json() # 사용자가 입력한 데이터
-#     target_item = body['userRequest']['utterance']
-#     ssstttrrr = target_item.split()
-
-#     # user=유저, money=잔액, item=주식, many=매수한 주식 수
-#     if len(ssstttrrr) >= 3 :
-#         item = "'%s'" % ssstttrrr[1]
-#         many = int(ssstttrrr[2])
-
-#     else :
-#         item = "'%s'" % ("갤럭시")
-#         many = 1
-        
-#     user = "'%s'" %str(body['userRequest']['user']['id'])
-#     target_item = item
-
-#     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-#     data = requests.get('https://kr.investing.com/search/?q=' + target_item, headers=headers)
-#     soup = BeautifulSoup(data.text, 'html.parser')
-
-#     try:
-#         item_code = soup.select_one('#fullColumn > div > div.js-section-wrapper.searchSection.allSection > div:nth-child(2) > div.js-inner-all-results-quotes-wrapper.newResultsContainer.quatesTable > a:nth-child(1) > span.second').text
-#         item_name = soup.select_one('#fullColumn > div > div.js-section-wrapper.searchSection.allSection > div:nth-child(2) > div.js-inner-all-results-quotes-wrapper.newResultsContainer.quatesTable > a:nth-child(1) > span.third').text
- 
-#         try:
-#             # # 국내 주식
-#             # # naver finance에서 추출
-#             target_data = web.DataReader(item_code, 'naver')
-#             last = (target_data.shape[0])
-#             target_data = target_data.iloc[last - 1].loc["Close"]
-
-#             # # 가격 추출 성공!
-#             item = "'%s'" % item_name
-#             cur.execute("SELECT * FROM game WHERE userid = %s;"% (user))
-#             rows = cur.fetchall()
-
-#             # # id가 없을 시 투자자금 1,000,000원 지급
-#             if len(rows) == 0 :
-#                 cur.execute("INSERT INTO game (userid, money) VALUES (%s, %d);"% (user, 1000000) )
-
-#             # # 돈만 있는 쿼리
-#             cur.execute("SELECT * FROM game WHERE userid = %s AND money IS NOT null;"% (user))
-#             rows2 = cur.fetchall()
-
-#             # # 종목명과 id로 검색한 쿼리
-#             cur.execute("SELECT * FROM game WHERE userid=%s AND item = %s;"% (user, item))
-#             rows3 = cur.fetchall()
-
-#             # 잔액이 0이상이면 실행
-#             money = int(rows2[0][1]) - int(target_data) * many
-#             if money >= 0 : 
-
-#                 # # 가지고 있는 주식 종목을 추가로 구매 
-#                 if len(rows3) != 0 :
-#                     shares = rows3[0][3] + many
-#                     cur.execute("UPDATE game SET shares=%d WHERE userid=%s AND item = %s;"% (shares, user, item) )
-#                     cur.execute("UPDATE game SET money=%d WHERE userid=%s AND money IS NOT null;"% (money, user) )
-#                     result =  str(many) + "주를 구매했습니다."
-                    
-#                 elif many <= 0:
-#                     result = "수량을 확인해주세요."
-
-#                 # # 가지고 있지 않은 주식을 구매
-#                 else :
-#                     cur.execute("INSERT INTO game (userid, item, shares) VALUES (%s, %s, %d);"% (user, item, many) )
-#                     cur.execute("UPDATE game SET money=%d WHERE userid=%s AND money IS NOT null;"% (money, user) )
-#                     result =  item_name + " " + str(many) + "주를 구매했습니다."
-#             else :
-#                 result = "잔액이 부족합니다"
-
-#             db.commit()
-
-#         except:
-#             result = "해외 주식은 아직 구현을 못했습니다."
-
-#     except AttributeError:
-#         result = "'" + target_item.strip() + "' 으로 검색결과가 없습니다.\n" + "찾는 주식이 있다면 주식명과 주가를 입력해주세요."
-
-#     responseBody = {
-#             "version": "2.0",
-#             "template": {
-#                 "outputs": [
-#                     {
-#                         "simpleText": {
-#                             "text": result
-#                         }
-#                     }
-#                 ]
-#             }
-#         }
-
-#     return responseBody
-
-# # # 모의주식 판매
-# @app.route('/api/sellitem', methods=['POST'])
-# def sellitem():
-
-#     body = request.get_json() # 사용자가 입력한 데이터
-#     target_item = body['userRequest']['utterance']
-#     ssstttrrr = target_item.split()
-
-#     # user=유저, money=잔액, item=주식, many=매수한 주식 수
-#     if len(ssstttrrr) >= 3 :
-#         item = "'%s'" % ssstttrrr[1]
-#         many = int(ssstttrrr[2])
-
-#     else :
-#         item = "'%s'" % ("갤럭시")
-#         many = 1
-        
-#     user = "'%s'" %str(body['userRequest']['user']['id'])
-#     target_item = item
-
-#     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-#     data = requests.get('https://kr.investing.com/search/?q=' + target_item, headers=headers)
-#     soup = BeautifulSoup(data.text, 'html.parser')
-
-#     try:
-#         item_code = soup.select_one('#fullColumn > div > div.js-section-wrapper.searchSection.allSection > div:nth-child(2) > div.js-inner-all-results-quotes-wrapper.newResultsContainer.quatesTable > a:nth-child(1) > span.second').text
-#         item_name = soup.select_one('#fullColumn > div > div.js-section-wrapper.searchSection.allSection > div:nth-child(2) > div.js-inner-all-results-quotes-wrapper.newResultsContainer.quatesTable > a:nth-child(1) > span.third').text
-
-#         try:
-#             # # 국내 주식
-#             # # naver finance에서 추출
-#             target_data = web.DataReader(item_code, 'naver')
-#             last = (target_data.shape[0])
-#             target_data = target_data.iloc[last - 1].loc["Close"]
-
-#             # # 가격 추출 성공!
-#             item = "'%s'" % item_name
-
-#             # # id와 종목명
-#             cur.execute("SELECT * FROM game WHERE userid=%s AND item = %s;"% (user, item))
-#             rows = cur.fetchall()
-
-#             if len(rows) != 0 :
-#                 limit = int(rows[0][3]) - int(many)
-
-#                 # # 돈만 있는 쿼리
-#                 cur.execute("SELECT * FROM game WHERE userid = %s AND money IS NOT null;"% (user))
-#                 rows = cur.fetchall()
-#                 harmoney = int(rows[0][1]) + int(target_data) * int(many)
-
-#                 if limit < 0 :
-#                     result = "보유 주식보다 더 많이 팔 수 없습니다."
-
-#                 elif limit == 0 :
-#                     # # money 컬럼 update
-#                     cur.execute("UPDATE game SET money = %d WHERE userid = %s AND money IS NOT null;" % (harmoney, user))
-                    
-#                     # # 해당 주식 행 삭제
-#                     cur.execute("DELETE FROM game WHERE userid = %s AND item = %s" % (user, item) )
-
-#                     result = item_name + " " + str(many) + "주를 모두 팔았습니다."
-#                 elif limit > 0 :
-#                     # # money 컬럼 update
-#                     cur.execute("UPDATE game SET money = %d WHERE userid = %s AND money IS NOT null;" % (harmoney, user))
-
-#                     # # 해당 주식 수 update
-#                     cur.execute("UPDATE game SET shares = %d WHERE userid = %s AND item = %s" % (limit, user, item)  )
-
-#                     result = item_name + " " + str(many) + "주를 팔았습니다."
-#                 else :
-#                     result = "알 수 없는 에러가 발생했습니다."
-#             else :
-#                 result = "보유하지 않은 주식은 판매 할 수 없습니다."
-
-#             db.commit()
-
-#         except:
-#             result = "해외 주식은 아직 구현을 못했습니다."
-
-#     except AttributeError:
-#         result = "'" + target_item.strip() + "' 으로 검색결과가 없습니다.\n" + "찾는 주식이 있다면 주식명만을 입력해주세요."
-
-#     responseBody = {
-#             "version": "2.0",
-#             "template": {
-#                 "outputs": [
-#                     {
-#                         "simpleText": {
-#                             "text": result
-#                         }
-#                     }
-#                 ]
-#             }
-#         }
-
-#     return responseBody
