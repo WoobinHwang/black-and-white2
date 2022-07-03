9ff61b5c866f405f772ae14d41875149563a55ed
WoobinHwang <dnqls0326@naver.com>
2022-07-03 13:01:37 +0900

    m

--- app/main.py
+++ app/main.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 
 # # NOS 서버 db
 passwd = 'b55d94be7d8dbef24e28a72a0dcb228fb48d1595665100e8da2cd1aafbe8bbbc'
-db = psycopg2.connect(host='ec2-54-204-56-171.compute-1.amazonaws.com', dbname='d2p5j2up8o05rg',user='dywzgxybcyjnzu',password= passwd,port=5432)
-cur=db.cursor()
+# db = psycopg2.connect(host='ec2-54-204-56-171.compute-1.amazonaws.com', dbname='d2p5j2up8o05rg',user='dywzgxybcyjnzu',password= passwd,port=5432)
+# cur=db.cursor()
 
 @app.route('/')
 def hello_world():
